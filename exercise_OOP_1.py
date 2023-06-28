90d4a02ec3b7f948259e467124db3b8eea3747f1
Viktor T <viktor.tretyakov.91@mail.ru>
2023-06-28 08:16:18 +0300

    Exercize_3

--- exercise_OOP_1.py
+++ exercise_OOP_1.py
@@ -17,6 +17,26 @@ class Student:
         else:
             return 'Ошибка'
 
+    def _average(self):
+        count = 0
+        quantity = 0
+        for rating in self.grades.values():
+            for rating_1 in rating:
+                count += rating_1
+                quantity += 1
+        return round((count / quantity), 1)
+
+    def __str__(self):
+        res = f'Имя = {self.name}\nФамилия = {self.surname}\nСредняя оценка за лекции: {self._average()}' \
+              f'\nКурсы в процессе изучения: {", ".join(self.courses_in_progress)}' \
+              f'\nЗавершенные курсы: {", ".join(self.finished_courses)}'
+        return res
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Нет студентов')
+            return
+        return self._average() < other._average()
 
 class Mentor:
     def __init__(self, name, surname):
@@ -29,6 +49,25 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+
+    def _average(self):
+        count = 0
+        quantity = 0
+        for rating in self.grades.values():
+            for rating_1 in rating:
+                count += rating_1
+                quantity += 1
+        return round((count / quantity), 1)
+
+    def __str__(self):
+        res = f'Имя = {self.name}\nФамилия = {self.surname}\nСредняя оценка за лекции: {self._average()}'
+        return res
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Нет лекторов')
+            return
+        return self._average() < other._average()
 
 
 class Reviewer(Mentor):
@@ -44,15 +83,55 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        res = f'Имя = {self.name}\nФамилия = {self.surname}'
+        return res
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
 
-cool_reviewer = Lecturer('Some', 'Buddy')
-cool_reviewer.courses_attached += ['Python']
+# Студенты
+some_student_1 = Student('Roy', 'Eman', 'your_gender')
+some_student_1.courses_in_progress += ['Python', 'Git']
+some_student_1.finished_courses += ['Введение в программирование']
+some_student_2 = Student('Arnold', 'Ewans', 'your_gender')
+some_student_2.courses_in_progress += ['Python', 'Git']
+some_student_2.finished_courses += ['Введение в программирование']
 
-best_student.rate_hw(cool_reviewer, 'Python', 10)
-best_student.rate_hw(cool_reviewer, 'Python', 10)
-best_student.rate_hw(cool_reviewer, 'Python', 10)
+# Менторы
+cool_mentor = Mentor('Irina', 'Masker')
+cool_mentor.courses_attached += ['Python']
 
-print(cool_reviewer.grades)
+# Лекторы
+some_lecturer_1 = Lecturer('Brain', 'Diz')
+some_lecturer_1.courses_attached += ['Python']
+some_lecturer_2 = Lecturer('Rou', 'Vinz')
+some_lecturer_2.courses_attached += ['Python']
+
+# Эксперты
+some_reviewer = Reviewer('Jon', 'Yoke')
+some_reviewer.courses_attached += ['Python', 'Git']
+
+# Оценки студентов
+some_reviewer.rate_hw(some_student_1, 'Python', 10)
+some_reviewer.rate_hw(some_student_1, 'Git', 7)
+some_reviewer.rate_hw(some_student_1, 'Python', 6)
+some_reviewer.rate_hw(some_student_2, 'Git', 5)
+some_reviewer.rate_hw(some_student_2, 'Python', 9)
+some_reviewer.rate_hw(some_student_2, 'Python', 6)
+
+# Оценки лекторов
+some_student_1.rate_hw(some_lecturer_1, 'Python', 9)
+some_student_1.rate_hw(some_lecturer_1, 'Python', 6)
+some_student_1.rate_hw(some_lecturer_1, 'Git', 4)
+some_student_2.rate_hw(some_lecturer_2, 'Python', 10)
+some_student_2.rate_hw(some_lecturer_2, 'Python', 7)
+some_student_2.rate_hw(some_lecturer_2, 'Git', 4)
+
+# print(some_student_1.grades)
+# print(some_student_2.grades)
+# print(some_reviewer)
+# print(some_student_1)
+# print(some_student_2)
+# print(some_student_1 > some_student_2)
+# print(some_lecturer_1)
+# print(some_lecturer_2)
+# print(some_lecturer_1 < some_lecturer_2)
